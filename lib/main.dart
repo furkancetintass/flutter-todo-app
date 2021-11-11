20c925a0f7c03b94815d0e91c51816c67f6f73c8
Furkan Çetintaş <furkan.cetintas@tsoft.com.tr>
2021-11-12 00:06:17 +0300

    geliştirmeler yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         theme: lightTheme,
         home: const SplashScreen(),
       ),
